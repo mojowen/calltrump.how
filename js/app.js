f16a25c6c6df8ae9376f79fc801b6cfdb42a0c0b
Scott Duncombe <sduncombe@renewfinancial.com>
2017-01-25 12:08:10 -0800

    Add count, new image, don't show called numbers

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 var url = "https://spreadsheets.google.com/feeds/list/1fj1BaowFV-ba2XIyjbdbllZzfwnqbqxUMQykP03171E/od6/public/basic?alt=json";
 
 var numbers = []
+var called = []
 
 var pics = ['phone.jpg',
             'phone2.png',
@@ -9,7 +10,8 @@ var pics = ['phone.jpg',
             'phone5.jpg',
             'phone6.jpg',
             'phone7.jpg',
-            'phone8.png']
+            'phone8.png',
+            'phone9.png']
 var pic = -1
 
 tinyGET(url, null, function(data) {
@@ -27,14 +29,19 @@ tinyGET(url, null, function(data) {
   }).filter(function(el) { return el.numberactive === 'Active' })
 
   anotherOne()
+
   document.getElementById('another').style.display = "block"
+  document.getElementById('made_calls').style.display = "inline-block"
+  document.getElementById('total').textContent = numbers.reduce(function(sum, el) {
+    return sum += parseInt(el.callsmade) }, 0)
 });
 
-function anotherOne() {
-  var current = document.getElementById('number').textContent
-      number = window.numbers.filter(function(el) {
-        return current !== el.phonenumber
-      })[Math.floor(Math.random() * (numbers.length - 1))]
+function anotherOne(did_call) {
+  if( !!did_call ) called.push(did_call)
+
+  var number = window.numbers.filter(function(el) {
+        return called.indexOf(el.phonenumber) === -1
+      })[Math.floor(Math.random() * (numbers.length - called.length))]
 
   document.getElementById('number').textContent = number.phonenumber
   document.getElementById('place').textContent = number.location
@@ -47,16 +54,18 @@ function anotherOne() {
 }
 
 function report(elem) {
-  var did_it_work = elem.getAttribute('answer')
+  var did_it_work = elem.getAttribute('answer'),
+      number = document.getElementById('number').textContent
 
   tinyPOST(
     'https://docs.google.com/forms/d/e/1FAIpQLSdgft-WNMlhPAsngZUl5idXhYPxQIEmHZzMw7QWwFZR0qA2ow/formResponse',
     {
-      'entry.1225496550': document.getElementById('number').textContent,
+      'entry.1225496550': number,
       'entry.1830234524': did_it_work
     }
   )
 
   alert('Thanks!')
+  anotherOne(number)
   return false
 }
